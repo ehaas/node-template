888314833856c43d924b2dcdfcadec708c82f692
Yoan Blanc <yoan@dosimple.ch>
2010-03-01 18:46:00 +0100

    Typo…

--- template.js
+++ template.js
@@ -16,21 +16,20 @@ var cache = {};
 var create = function(file_name, callback) {
     fs.readFile(file_name).addCallback(function(file_contents) {
         callback(new Function("obj",
-             "var p=[],print=function(){p.push.apply(p,arguments);};" +
+            "var p=[],print=function(){p.push.apply(p,arguments);};" +
+            "obj=obj||{};" +
+            // Introduce the data as local variables using with(){}
+            "with(obj){p.push('" +
 
-             // Introduce the data as local variables using with(){}
-             "with(obj){p.push('" +
-
-             // Convert the template into pure JavaScript
-             file_contents
-             .replace(/[\r\n\t]/g, " ")
-             .split("<%").join("\t")
-             .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-                     .replace(/\t=(.*?)%>/g, "',$1,'")
-                     .split("\t").join("');")
-                     .split("%>").join("p.push('")
-                     .split("\r").join("\\'")
-                     + "');}return p.join('');"));
+            // Convert the template into pure JavaScript
+            file_contents.replace(/[\r\n\t]/g, " ")
+                         .split("<%").join("\t")
+                         .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+                         .replace(/\t=(.*?)%>/g, "',$1,'")
+                         .split("\t").join("');")
+                         .split("%>").join("p.push('")
+                         .split("\r").join("\\'") +
+        "');}return p.join('');"));
     }).addErrback(function(e) {
         // do something to handle the error
     });
