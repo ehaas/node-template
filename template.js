ae1c2afca2ba63c837688ef23578719675905a54
nico tübingen <nicolasmondon@gmail.com>
2010-03-01 08:32:47 -0700

    update for node.js 0.1.30

--- template.js
+++ template.js
@@ -9,40 +9,31 @@
  */
 
 
-var posix = require("posix");
+var fs = require("fs");
 
 var cache = {};
   
-var tmpl  = function (str, data){
-    // Figure out if we're getting a template, or if we need to
-    // load the template - and be sure to cache the result.
-    var fn = !/[\t\r\n% ]/.test(str) ?
-    cache[str] = cache[str] ||
-    // read the template from the file specified
-    tmpl(posix.cat(str).wait()) :
-    
-    // Generate a reusable function that will serve as a template
-    // generator (and which will be cached).
-    new Function("obj",
-		 "var p=[],print=function(){p.push.apply(p,arguments);};" +
-		 
-		 // Introduce the data as local variables using with(){}
-		 "with(obj){p.push('" +
-		 
-		 // Convert the template into pure JavaScript
-		 str
-		 .replace(/[\r\n\t]/g, " ")
-		 .split("<%").join("\t")
-		 .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-                 .replace(/\t=(.*?)%>/g, "',$1,'")
-                 .split("\t").join("');")
-                 .split("%>").join("p.push('")
-                 .split("\r").join("\\'")
-                 + "');}return p.join('');");
-    // Provide some basic currying to the user
-    return data ? fn( data ) : fn;
-};
+var create = function(file_name, callback) {
+	fs.readFile(file_name, function(err, data){
+		if(err) throw err;
+		callback(new Function("obj",
+		         "var p=[],print=function(){p.push.apply(p,arguments);};" +
 
+		         // Introduce the data as local variables using with(){}
+		         "with(obj){p.push('" +
+
+		         // Convert the template into pure JavaScript
+		         data
+		         .replace(/[\r\n\t]/g, " ")
+		         .split("<%").join("\t")
+		         .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+		                 .replace(/\t=(.*?)%>/g, "',$1,'")
+		                 .split("\t").join("');")
+		                 .split("%>").join("p.push('")
+		                 .split("\r").join("\\'")
+		                 + "');}return p.join('');"));
+	});
+}
 
 /* exports */
-exports.tmpl = tmpl;
+exports.create = create;
